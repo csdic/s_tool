902aaf713ad3641acbb91a0be15dcaef5fd6c355
kkjangi <kk.jangi@gmail.com>
2018-08-03 11:50:54 +0900

    url 파일 수정

--- regulation/views.py
+++ regulation/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
 
+# User define module
+from .models import RegulationBody
 
 # create your views here
 
 def regulation_text(request):
-    return render(request, 'regulation/regulation_text.html', {})
+    r_contents = RegulationBody.objects.all()
+    return render(request, 'regulation/regulation_text.html', {'r_contents':r_contents})
 
